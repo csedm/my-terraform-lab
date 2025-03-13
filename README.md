2b7fac8b7abffab14d837aede18e74b4c62e4713
csedm <9305777+csedm@users.noreply.github.com>
2025-03-13 15:58:52 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# my-terraform-lab
+A basic terraform lab deployment for experimentation and demonstration of terraform skillset.
---
+++ terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.11.0"
+  cloud {
+      organization = "my-terraform-lab-csedm"
+      workspaces {
+        name = "my-terraform-lab"
+      }
+  }
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.90.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
