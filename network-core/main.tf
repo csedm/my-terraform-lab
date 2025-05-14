fa63060e2127f70c6147a7e5a0c45527d04ebe9f
csedm <9305777+csedm@users.noreply.github.com>
2025-05-13 23:01:00 -0400

    add ipv6 ingress rules

--- network-core/main.tf
+++ network-core/main.tf
@@ -107,7 +107,7 @@ resource "aws_instance" "bastion" {
 
 # Security Groups - bastion
 resource "aws_security_group" "bastion-sg" {
-  name        = "${random_pet.bastion_name.id}-sg"
+  name        = "bastion-sg"
   description = "allow inbound ssh traffic"
   vpc_id      = aws_vpc.mytf.id
   lifecycle {
@@ -122,8 +122,22 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ingress_ssh_ipv4" {
   to_port           = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_ingress_ssh_ipv6" {
+  security_group_id = aws_security_group.bastion-sg.id
+  cidr_ipv6         = "::/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_bastion_traffic_ipv4" {
   security_group_id = aws_security_group.bastion-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
+}
+resource "aws_vpc_security_group_egress_rule" "allow_all_bastion_traffic_ipv6" {
+  security_group_id = aws_security_group.bastion-sg.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1"
+  
 }
