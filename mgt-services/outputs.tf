6b1a582440201bb8c4faf9ec2f37b9a7d7fdf1fe
csedm <9305777+csedm@users.noreply.github.com>
2025-06-07 22:31:17 -0400

    add ssm bastion param output

--- mgt-services/outputs.tf
+++ mgt-services/outputs.tf
@@ -16,3 +16,21 @@ output "bastion_public_ipv6_address" {
 output "bastion_security_group_id" {
   value = aws_security_group.bastion-sg.id
 }
+
+module "ssm_parameters" {
+  source = "cloudposse/ssm-parameter-store/aws"
+  version = "0.13.0"
+
+  parameter_write = [
+    {
+      name = "/${var.origin_repo}/${local.environment}/bastion_host"
+      value = aws_instance.bastion.public_ip
+      type = "String"
+    },
+    {
+      name = "/${var.origin_repo}/${local.environment}/bastion_host_ipv6"
+      value = aws_instance.bastion.ipv6_addresses[0]
+      type = "String"
+    }
+  ]
+}
