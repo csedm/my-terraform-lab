79cda71338d04ebd4d517f32ac0f2b87a5f5981d
csedm <9305777+csedm@users.noreply.github.com>
2025-07-03 06:28:09 -0400

    add ecs-cluster configuration (#8)
    * add initial commit of ecs-cluster configuration
    
    * Refactor IAM policies for ECS: least privilege for SSM and CloudWatch logs, dynamic account/region, and expanded log group scope

---
+++ ecs-cluster/variables.tf
@@ -0,0 +1,48 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+variable "origin_repo" {
+  description = "Originating repository"
+  type        = string
+  default     = "my-terraform-lab"
+}
+variable "tfe_organization" {
+  description = "TFE organization to use for querying HCP Terraform remote state data."
+  nullable    = false
+}
+variable "tfe_token" {
+  description = "Token to provide to TFE provider needed to query remote state resources."
+  nullable    = false
+  sensitive   = true
+}
+
+variable "network_core_workspace_base_name" {
+  description = "The base name of the HCP Terraform workspace for the network core. The environment will be appended."
+  type        = string
+}
+
+variable "cloudflare_account_id" {
+  description = "Cloudflare account ID"
+  type        = string
+  sensitive   = true
+}
+variable "cloudflare_api_token" {
+  description = "Cloudflare API token to use with cloudflare provider"
+  type        = string
+  sensitive   = true
+}
+variable "cloudflare_domain" {
+  description = "cloudflare base domain"
+  type        = string
+}
+variable "cloudflare_zone_id" {
+  description = "cloudflare domain zone id"
+  type        = string
+}
+
+# variable "network_core_workspace_base_name" {
+#   description = "The base name of the HCP Terraform workspace for the network core. The environment will be appended."
+#   type        = string
+# }
